2561eab3df3f662787b5614c38eadaa0e3e86ba8
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-01-07 11:01:01 +0100

    init

---
+++ src/test/java/tech/jhipster/beer/BeerAppIT.java
@@ -0,0 +1,120 @@
+package tech.jhipster.beer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static tech.jhipster.beer.BeerApp.LF;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+@IntegrationTest
+class BeerAppIT {
+
+  @Test
+  void shouldMain() {
+    assertThatCode(() -> BeerApp.main(new String[] {})).doesNotThrowAnyException();
+  }
+
+  @Test
+  void shouldApplicationRunning() {
+    String result = BeerApp.applicationRunning("jhlite");
+    assertThat(result).isEqualTo("  Application 'jhlite' is running!" + LF);
+  }
+
+  @Test
+  void shouldAccessUrlLocalWithoutServerPort() {
+    String result = BeerApp.accessUrlLocal(null, null, null);
+    assertThat(result).isEmpty();
+  }
+
+  @Test
+  void shouldAccessUrlLocalWithoutContextPath() {
+    String result = BeerApp.accessUrlLocal("http", "8080", "/");
+    assertThat(result).isEqualTo("  Local: \thttp://localhost:8080/" + LF);
+  }
+
+  @Test
+  void shouldAccessUrlLocalWithContextPath() {
+    String result = BeerApp.accessUrlLocal("http", "8080", "/lite/");
+    assertThat(result).isEqualTo("  Local: \thttp://localhost:8080/lite/" + LF);
+  }
+
+  @Test
+  void shouldAccessUrlExternalWithoutServerPort() {
+    String result = BeerApp.accessUrlExternal(null, null, null, null);
+    assertThat(result).isEmpty();
+  }
+
+  @Test
+  void shouldAccessUrlExternalWithoutContextPath() {
+    String result = BeerApp.accessUrlExternal("http", "127.0.1.1", "8080", "/");
+    assertThat(result).isEqualTo("  External: \thttp://127.0.1.1:8080/" + LF);
+  }
+
+  @Test
+  void shouldAccessUrlExternalWithContextPath() {
+    String result = BeerApp.accessUrlExternal("http", "127.0.1.1", "8080", "/lite/");
+    assertThat(result).isEqualTo("  External: \thttp://127.0.1.1:8080/lite/" + LF);
+  }
+
+  @Test
+  void shouldConfigServerWithoutConfigServerStatus() {
+    String result = BeerApp.configServer(null);
+    assertThat(result).isEmpty();
+  }
+
+  @Test
+  void shouldConfigServer() {
+    String result = BeerApp.configServer("Connected to the JHipster Registry running in Docker");
+    assertThat(result).contains("Config Server: Connected to the JHipster Registry running in Docker");
+  }
+
+  @Test
+  void shouldGetProtocol() {
+    assertThat(BeerApp.getProtocol(null)).isEqualTo("http");
+  }
+
+  @Test
+  void shouldGetProtocolForBlank() {
+    assertThat(BeerApp.getProtocol(" ")).isEqualTo("https");
+  }
+
+  @Test
+  void shouldGetProtocolForValue() {
+    assertThat(BeerApp.getProtocol("https")).isEqualTo("https");
+  }
+
+  @Test
+  void shouldGetContextPath() {
+    assertThat(BeerApp.getContextPath("/chips")).isEqualTo("/chips");
+  }
+
+  @Test
+  void shouldGetContextPathForNull() {
+    assertThat(BeerApp.getContextPath(null)).isEqualTo("/");
+  }
+
+  @Test
+  void shouldGetContextPathForBlank() {
+    assertThat(BeerApp.getContextPath(" ")).isEqualTo("/");
+  }
+
+  @Test
+  void shouldGetHost() {
+    assertThatCode(BeerApp::getHostAddress).doesNotThrowAnyException();
+  }
+
+  @Test
+  void shouldGetHostWithoutHostAddress() {
+    try (MockedStatic<InetAddress> inetAddress = Mockito.mockStatic(InetAddress.class)) {
+      inetAddress.when(InetAddress::getLocalHost).thenThrow(new UnknownHostException());
+
+      String result = BeerApp.getHostAddress();
+
+      assertThat(result).isEqualTo("localhost");
+    }
+  }
+}
